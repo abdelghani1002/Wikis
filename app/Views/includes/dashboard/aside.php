02782635110fbc86290bfc51cf2a9f5c38d38e42
abdelghaniAit <aaittamghart8@gmail.com>
2024-01-09 10:50:01 +0100

    Modify asidebar icons

--- app/Views/includes/dashboard/aside.php
+++ app/Views/includes/dashboard/aside.php
@@ -2,6 +2,7 @@
     <div class="h-full px-3 py-4 overflow-y-auto dark:bg-gray-800">
         <ul class="h-full font-medium flex flex-col justify-between">
             <div class="space-y-2">
+                <!-- Home -->
                 <li>
                     <a href="<?= $_ENV['APP_URL'] ?>" class="home flex flex-row-reverse md:flex-row justify-between md:justify-normal items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-200 dark:hover:bg-gray-700 group">
                         <div class="">
@@ -19,6 +20,7 @@
                     </a>
                 </li>
 
+                <!-- Statisctics -->
                 <li>
                     <a href="<?= $_ENV['APP_URL'] . "/dashboard" ?>" class="statistics flex flex-row-reverse md:flex-row justify-between gap-2 md:gap-0 md:justify-normal items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-200 dark:hover:bg-gray-700 group">
                         <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 18">
@@ -31,10 +33,22 @@
                     </a>
                 </li>
 
+                <!-- Categories -->
                 <li>
                     <a href="<?= $_ENV['APP_URL'] . "/dashboard/categories" ?>" class="categories flex flex-row-reverse md:flex-row justify-between md:justify-normal items-center p-2 text--900 rounded-lg dark:text-white hover:bg-gray-200 dark:hover:bg-gray-700 group">
-                        <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 18">
-                            <path d="M10.5 2.5C10.5 3.16304 10.2366 3.79893 9.76777 4.26777C9.29893 4.73661 8.66304 5 8 5C7.33696 5 6.70107 4.73661 6.23223 4.26777C5.76339 3.79893 5.5 3.16304 5.5 2.5C5.5 1.83696 5.76339 1.20107 6.23223 0.732233C6.70107 0.263392 7.33696 0 8 0C8.66304 0 9.29893 0.263392 9.76777 0.732233C10.2366 1.20107 10.5 1.83696 10.5 2.5ZM15.5 3C15.5 3.53043 15.2893 4.03914 14.9142 4.41421C14.5391 4.78929 14.0304 5 13.5 5C12.9696 5 12.4609 4.78929 12.0858 4.41421C11.7107 4.03914 11.5 3.53043 11.5 3C11.5 2.46957 11.7107 1.96086 12.0858 1.58579C12.4609 1.21071 12.9696 1 13.5 1C14.0304 1 14.5391 1.21071 14.9142 1.58579C15.2893 1.96086 15.5 2.46957 15.5 3ZM2.5 5C3.03043 5 3.53914 4.78929 3.91421 4.41421C4.28929 4.03914 4.5 3.53043 4.5 3C4.5 2.46957 4.28929 1.96086 3.91421 1.58579C3.53914 1.21071 3.03043 1 2.5 1C1.96957 1 1.46086 1.21071 1.08579 1.58579C0.710714 1.96086 0.5 2.46957 0.5 3C0.5 3.53043 0.710714 4.03914 1.08579 4.41421C1.46086 4.78929 1.96957 5 2.5 5ZM4 7.25C4 6.56 4.56 6 5.25 6H10.75C11.44 6 12 6.56 12 7.25V12C12 13.0609 11.5786 14.0783 10.8284 14.8284C10.0783 15.5786 9.06087 16 8 16C6.93913 16 5.92172 15.5786 5.17157 14.8284C4.42143 14.0783 4 13.0609 4 12V7.25ZM3 7.25C3 6.787 3.14 6.358 3.379 6H1.25C0.56 6 1.78381e-08 6.56 1.78381e-08 7.25V11C-4.66659e-05 11.4281 0.0915386 11.8513 0.268602 12.2411C0.445665 12.6309 0.704104 12.9782 1.02655 13.2599C1.34901 13.5415 1.728 13.7508 2.13807 13.8738C2.54813 13.9968 2.97978 14.0307 3.404 13.973C3.13645 13.3497 2.99898 12.6783 3 12V7.25ZM13 12C13 12.7 12.856 13.368 12.596 13.973C13.0202 14.0307 13.4519 13.9968 13.8619 13.8738C14.272 13.7508 14.651 13.5415 14.9734 13.2599C15.2959 12.9782 15.5543 12.6309 15.7314 12.2411C15.9085 11.8513 16 11.4281 16 11V7.25C16 6.56 15.44 6 14.75 6H12.621C12.861 6.358 13 6.787 13 7.25V12Z" />
+                        <svg width="19px" height="20px" viewBox="0 0 19 20" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+                            <g id="Icons" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                                <g id="Rounded" transform="translate(-614.000000, -3124.000000)">
+                                    <g id="Maps" transform="translate(100.000000, 3068.000000)">
+                                        <g id="-Round-/-Maps-/-category" transform="translate(511.000000, 54.000000)">
+                                            <g>
+                                                <polygon id="Path" points="0 0 24 0 24 24 0 24"></polygon>
+                                                <path d="M11.15,3.4 C11.54,2.76 12.46,2.76 12.85,3.4 L16.56,9.48 C16.97,10.14 16.49,11 15.71,11 L8.28,11 C7.5,11 7.02,10.14 7.43,9.48 L11.15,3.4 Z M17.5,22 C15.0147186,22 13,19.9852814 13,17.5 C13,15.0147186 15.0147186,13 17.5,13 C19.9852814,13 22,15.0147186 22,17.5 C22,19.9852814 19.9852814,22 17.5,22 Z M4,21.5 C3.45,21.5 3,21.05 3,20.5 L3,14.5 C3,13.95 3.45,13.5 4,13.5 L10,13.5 C10.55,13.5 11,13.95 11,14.5 L11,20.5 C11,21.05 10.55,21.5 10,21.5 L4,21.5 Z" id="🔹-Icon-Color" fill="#708090"></path>
+                                            </g>
+                                        </g>
+                                    </g>
+                                </g>
+                            </g>
                         </svg>
 
                         <span class="flex-1 m-0 md:ms-3 whitespace-nowrap">Categories</span>
@@ -43,8 +57,9 @@
 
                 <li>
                     <a href="<?= $_ENV['APP_URL'] . "/dashboard/tags" ?>" class="tags flex flex-row-reverse md:flex-row justify-between md:justify-normal items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-200 dark:hover:bg-gray-700 group">
-                        <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 18">
-                            <path d="M14 2a3.963 3.963 0 0 0-1.4.267 6.439 6.439 0 0 1-1.331 6.638A4 4 0 1 0 14 2Zm1 9h-1.264A6.957 6.957 0 0 1 15 15v2a2.97 2.97 0 0 1-.184 1H19a1 1 0 0 0 1-1v-1a5.006 5.006 0 0 0-5-5ZM6.5 9a4.5 4.5 0 1 0 0-9 4.5 4.5 0 0 0 0 9ZM8 10H5a5.006 5.006 0 0 0-5 5v2a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-2a5.006 5.006 0 0 0-5-5Z" />
+                        <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-tag">
+                            <path d="M20.59 13.41l-7.17 7.17a2 2 0 0 1-2.83 0L2 12V2h10l8.59 8.59a2 2 0 0 1 0 2.82z" id="id_101" style="stroke: rgb(112, 128, 144);"></path>
+                            <line x1="7" y1="7" x2="7" y2="7" id="id_102" style="stroke: rgb(112, 128, 144);"></line>
                         </svg>
                         <span class="flex-1 m-0 md:ms-3 whitespace-nowrap">Tags</span>
                     </a>
@@ -52,8 +67,18 @@
 
                 <li>
                     <a href="<?= $_ENV['APP_URL'] . "/dashboard/wikis" ?>" class="wikis flex flex-row-reverse md:flex-row justify-between md:justify-normal items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-200 dark:hover:bg-gray-700 group">
-                        <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 18">
-                            <path d="M14 2a3.963 3.963 0 0 0-1.4.267 6.439 6.439 0 0 1-1.331 6.638A4 4 0 1 0 14 2Zm1 9h-1.264A6.957 6.957 0 0 1 15 15v2a2.97 2.97 0 0 1-.184 1H19a1 1 0 0 0 1-1v-1a5.006 5.006 0 0 0-5-5ZM6.5 9a4.5 4.5 0 1 0 0-9 4.5 4.5 0 0 0 0 9ZM8 10H5a5.006 5.006 0 0 0-5 5v2a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-2a5.006 5.006 0 0 0-5-5Z" />
+                        <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" xmlns="http://www.w3.org/2000/svg" xml:space="preserve" width="1.70666in" height="1.70666in" version="1.1" style="shape-rendering:geometricPrecision; text-rendering:geometricPrecision; image-rendering:optimizeQuality; fill-rule:evenodd; clip-rule:evenodd" viewBox="0 0 1707 1707" xmlns:xlink="http://www.w3.org/1999/xlink">
+                            <defs>
+                                <style type="text/css">
+                                    .fil0 {
+                                        fill: #000002
+                                    }
+                                </style>
+                            </defs>
+                            <g id="Layer_x0020_1">
+                                <metadata id="CorelCorpID_0Corel-Layer"></metadata>
+                                <path class="fil0" d="M1500 219l-576 0c-30,0 -30,-46 0,-46l576 0c29,0 29,46 0,46zm184 1488l-1661 0c-13,0 -23,-11 -23,-23l0 -1661c0,-13 10,-23 23,-23l1661 0c12,0 23,10 23,23l0 1661c0,12 -11,23 -23,23zm-1638 -46l1615 0 0 -1270 -1615 0 0 1270zm1615 -1315l0 -300 -1615 0 0 300 1615 0zm-108 1091l-1371 0c-30,0 -30,-45 0,-45l1371 0c30,0 30,45 0,45zm-214 138l-943 0c-30,0 -30,-46 0,-46l943 0c30,0 30,46 0,46zm-697 -275c-8,0 -16,-5 -20,-13l-388 -776c-13,-27 27,-47 41,-21l367 736 200 -399 -158 -316c-13,-27 27,-47 41,-21l142 286 143 -286c14,-26 54,-6 41,21l-158 316 199 399 368 -736c14,-26 54,-6 41,21l-388 776c-9,17 -33,17 -41,0l-205 -409 -204 409c-4,8 -12,13 -21,13zm-381 -1028c-42,0 -77,-34 -77,-76 0,-42 35,-77 77,-77 42,0 76,35 76,77 0,42 -34,76 -76,76zm0 -107c-41,0 -41,62 0,62 41,0 41,-62 0,-62zm217 107c-42,0 -76,-34 -76,-76 0,-42 34,-77 76,-77 42,0 77,35 77,77 0,42 -35,76 -77,76zm0 -107c-41,0 -41,62 0,62 41,0 41,-62 0,-62zm217 107c-42,0 -76,-34 -76,-76 0,-42 34,-77 76,-77 43,0 77,35 77,77 0,42 -34,76 -77,76zm0 -107c-40,0 -40,62 0,62 41,0 41,-62 0,-62z" id="id_101" style="fill: rgb(112, 128, 144);"></path>
+                            </g>
                         </svg>
                         <span class="flex-1 m-0 md:ms-3 whitespace-nowrap">Wikis</span>
                     </a>
