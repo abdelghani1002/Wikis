ec9c318d07c359a5cc162abb1ccb023e291553db
abdelghaniAit <aaittamghart8@gmail.com>
2024-01-09 16:09:24 +0100

    Upgrade URI catch

--- index.php
+++ index.php
@@ -8,5 +8,5 @@ require('core/routes.php');
 $uri = $_SERVER['REQUEST_URI'];
 
 $uri = str_replace('/' . $_ENV['APP_NAME'], '', $uri);
-$uri = rtrim($uri, '/');
+$uri[strlen($uri) - 1] === '/' && $uri !== "/" ? $uri = rtrim($uri, '/') : null;
 $router->dispatch($uri);
