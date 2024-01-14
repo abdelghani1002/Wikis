b730fe31b9fcdfaa3dc43d399b1b0a12f9a91c2e
abdelghaniAit <aaittamghart8@gmail.com>
2024-01-15 00:34:13 +0100

    Add statistics to dashboard index page

--- Views/dashboard/index.php
+++ Views/dashboard/index.php
@@ -30,58 +30,58 @@
             <div class="p-2 rounded-lg dark:border-gray-700 flex flex-col grow justify-between h-full">
                 <div class="grid grid-cols-3 gap-4 mb-4">
                     <div class="flex flex-col grow items-center bg-green-500 justify-center h-24 rounded dark:bg-gray-800">
-                        <p class="text-md md:text-2xl  text-gray-100 dark:text-gray-500">
-                            200%
+                        <p class="text-md md:text-3xl font-extrabold text-gray-100 dark:text-gray-500">
+                            <?= $authors_count ?>
                         </p>
-                        <p class="text-md md:text-2xl  text-gray-100 dark:text-gray-500">
-                            a statistic
-                        </p>
-
-                    </div>
-                    <div class="flex flex-col grow items-center bg-green-500 justify-center h-24 rounded dark:bg-gray-800">
-                        <p class="text-md md:text-2xl  text-gray-100 dark:text-gray-500">
-                            200%
-                        </p>
-                        <p class="text-md md:text-2xl  text-gray-100 dark:text-gray-500">
-                            a statistic
+                        <p class="text-md md:text-2xl text-gray-100 dark:text-gray-500">
+                            authors
                         </p>
 
                     </div>
                     <div class="flex flex-col grow items-center bg-green-500 justify-center h-24 rounded dark:bg-gray-800">
-                        <p class="text-md md:text-2xl  text-gray-100 dark:text-gray-500">
-                            200%
+                        <p class="text-md md:text-3xl font-extrabold text-gray-100 dark:text-gray-500">
+                            <?= $categories_count ?>
                         </p>
-                        <p class="text-md md:text-2xl  text-gray-100 dark:text-gray-500">
-                            a statistic
+                        <p class="text-md md:text-2xl text-gray-100 dark:text-gray-500">
+                            categories
+                        </p>
+
+                    </div>
+                    <div class="flex flex-col grow items-center bg-green-500 justify-center h-24 rounded dark:bg-gray-800">
+                        <p class="text-md md:text-3xl font-extrabold text-gray-100 dark:text-gray-500">
+                            <?= $tags_count ?>
+                        </p>
+                        <p class="text-md md:text-2xl text-gray-100 dark:text-gray-500">
+                            tags
                         </p>
 
                     </div>
                 </div>
 
                 <div class="flex flex-col grow bg-green-500 items-center justify-center h-48 mb-4 rounded dark:bg-gray-800">
-                    <p class="text-md md:text-2xl  text-gray-100 dark:text-gray-500">
+                    <p class="text-md md:text-3xl font-extrabold text-gray-100 dark:text-gray-500">
                         100%
                     </p>
-                    <p class="text-md md:text-2xl  text-gray-100 dark:text-gray-500">
+                    <p class="text-md md:text-2xl text-gray-100 dark:text-gray-500">
                         important
                     </p>
                 </div>
 
                 <div class="grid grid-cols-2 gap-4">
                     <div class="flex flex-col grow items-center justify-center rounded bg-green-500 h-28 dark:bg-gray-800">
-                        <p class="text-md md:text-2xl  text-gray-100 dark:text-gray-500">
-                            63%
+                        <p class="text-md md:text-3xl font-extrabold text-gray-100 dark:text-gray-500">
+                            <?= $wikis_count ?>
                         </p>
-                        <p class="text-md md:text-2xl  text-gray-100 dark:text-gray-500">
-                            other statistic
+                        <p class="text-md md:text-2xl text-gray-100 dark:text-gray-500">
+                            published wikis
                         </p>
                     </div>
                     <div class="flex flex-col grow items-center justify-center rounded bg-green-500 h-28 dark:bg-gray-800">
-                        <p class="text-md md:text-2xl  text-gray-100 dark:text-gray-500">
-                            63%
+                        <p class="text-md md:text-3xl font-extrabold text-gray-100 dark:text-gray-500">
+                            <?= $archived_wikis_count ?>
                         </p>
-                        <p class="text-md md:text-2xl  text-gray-100 dark:text-gray-500">
-                            other statistic
+                        <p class="text-md md:text-2xl text-gray-100 dark:text-gray-500">
+                            archived wikis
                         </p>
                     </div>
                 </div>
--- app/Controllers/AdminController.php
+++ app/Controllers/AdminController.php
@@ -2,12 +2,29 @@
 
 namespace app\Controllers;
 
+use app\Models\Category;
+use app\Models\Tag;
+use app\Models\User;
+use app\Models\Wiki;
+
 class AdminController extends Controller
 {
     public function dashboard()
     {
         if (AuthController::user() && AuthController::user()['role'] === "admin") {
-            $this->render('dashboard/index');
+            $wikis_count = count(Wiki::select("status = 'published'"));
+            $archived_wikis_count = count(Wiki::select("status = 'archived'"));
+            $categories_count = count(Category::select());
+            $tags_count = count(Tag::select());
+            $authors_count = count(User::select("role = 'author'"));
+            $data = [
+                'wikis_count' => $wikis_count,
+                'archived_wikis_count' => $archived_wikis_count,
+                'categories_count' => $categories_count,
+                'tags_count' => $tags_count,
+                'authors_count' => $authors_count,
+            ];
+            $this->render('dashboard/index', $data);
         } else {
             header('location: ' . $_ENV['APP_URL']);
         }
