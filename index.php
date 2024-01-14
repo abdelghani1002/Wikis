755fce2602347f623cb371fdbcf05919c289fd60
abdelghaniAit <aaittamghart8@gmail.com>
2024-01-14 21:40:16 +0100

    Modify index for catch the uri without GET params

--- index.php
+++ index.php
@@ -7,6 +7,12 @@ require('core/routes.php');
 
 $uri = $_SERVER['REQUEST_URI'];
 
+// Find the position of the last occurrence of '?'
+$lastQuestionMark = strrpos($uri, '?');
+
+// If '?' is found, extract the substring up to that position; otherwise, use the entire URI
+$uri = ($lastQuestionMark !== false) ? substr($uri, 0, $lastQuestionMark) : $uri;
+
 $uri = str_replace('/' . $_ENV['APP_NAME'], '', $uri);
 $uri[strlen($uri) - 1] === '/' && $uri !== "/" ? $uri = rtrim($uri, '/') : null;
 $router->dispatch($uri);
