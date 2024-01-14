a042061f655b34021e92c5a75b53a7ae33a952fb
abdelghaniAit <aaittamghart8@gmail.com>
2024-01-14 22:38:29 +0100

    Improve profle Controle

--- app/Controllers/UserController.php
+++ app/Controllers/UserController.php
@@ -2,12 +2,24 @@
 
 namespace app\Controllers;
 
+use app\Models\User;
+use app\Models\Wiki;
+
 class UserController extends Controller
 {
     public function profile()
     {
         if (AuthController::user() && AuthController::user()['role'] === "author") {
-            $this->render('profile');
+            $user = User::select("id = " . AuthController::user()["id"]);
+            $data = ['user' => $user];
+            $wikis = Wiki::select("author_id = " . $user['id']);
+            if (isset($wikis['id'])) $wikis = [$wikis];
+            if(isset($wikis['id'])){
+                $data['wikis'] = [$wikis];
+            }else{
+                $data['wikis'] = $wikis;
+            }
+            $this->render('profile', $data);
         } else {
             header('location: ' . $_ENV['APP_URL']);
         }
