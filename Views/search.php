30eb25c7033f027e14e2c7ca71f0b2ce2732349e
abdelghaniAit <aaittamghart8@gmail.com>
2024-01-16 10:26:33 +0100

    Modify search page

--- Views/search.php
+++ Views/search.php
@@ -105,17 +105,17 @@ if (session_status() == PHP_SESSION_NONE) {
 
                         div.innerHTML = `
                         <a href="<?= $_ENV['APP_URL'] . "/wikis/show?id=" ?>${wiki.id}" class="w-full max-h-32">
-                            <img class="w-full h-full rounded-t-lg object-cover" src="<?= $_ENV['APP_URL'] . $wiki['wiki_photo_src'] ?>" alt="wiki" />
+                            <img class="w-full h-full rounded-t-lg object-cover" src="<?= $_ENV['APP_URL'] ?>${wiki.wiki_photo_src}" alt="wiki" />
                         </a>
                         <div class="p-5 overflow-hidden max-h-52">
                             <h5 class="mb-2 text-xl font-medium leading-tight text-neutral-800 dark:text-neutral-50">
-                                ${wiki.title} ?>
+                                ${wiki.title}
                             </h5>
                             <h5 class="mb-2 text-l leading-tight text-neutral-500 dark:text-neutral-50">
-                                ${wiki.category_name} ?>
+                                ${wiki.category_name}
                             </h5>
                             <p class="overflow-hidden text-base text-neutral-600 dark:text-neutral-200">
-                                ${wiki.content} ?>
+                                ${wiki.content}
                             </p>
                         </div>
                         <div class="p-3 flex justify-end">
@@ -127,7 +127,7 @@ if (session_status() == PHP_SESSION_NONE) {
                             </a>
                         </div>
                         <div class="mt-auto border-t-2 border-neutral-100 px-6 py-3 dark:border-neutral-600 dark:text-neutral-50">
-                            <small>Posted ${wiki.wiki_created_at} ?></small>
+                            <small>Posted ${wiki.wiki_created_at}</small>
                         </div>`;
 
                         parent.appendChild(div);
@@ -163,17 +163,17 @@ if (session_status() == PHP_SESSION_NONE) {
                         </a>
                         <div class="p-5 overflow-hidden max-h-52">
                             <h5 class="mb-2 text-xl font-medium leading-tight text-neutral-800 dark:text-neutral-50">
-                                ${wiki.title} ?>
+                                ${wiki.title}
                             </h5>
                             <h5 class="mb-2 text-l leading-tight text-neutral-500 dark:text-neutral-50">
-                                ${wiki.category_name} ?>
+                                ${wiki.category_name}
                             </h5>
                             <p class="overflow-hidden text-base text-neutral-600 dark:text-neutral-200">
-                                ${wiki.content} ?>
+                                ${wiki.content}
                             </p>
                         </div>
                         <div class="p-3 flex justify-end">
-                            <a href="<?= $_ENV['APP_URL'] . "/wikis/show?id=" ?>${wiki.id}" class="text-blue-800 border border-blue-800 hover:text-white hover:bg-blue-800 py-2 px-3 rounded inline-flex  items-center">
+                            <a href="<?= $_ENV['APP_URL'] . "/wikis/show?id="   ?> ${wiki.id}" class="text-blue-800 border border-blue-800 hover:text-white hover:bg-blue-800 py-2 px-3 rounded inline-flex  items-center">
                                 View wiki
                                 <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-6 h-6 ml-2">
                                     <path d="M14 5l7 7m0 0l-7 7m7-7H3"></path>
@@ -181,7 +181,7 @@ if (session_status() == PHP_SESSION_NONE) {
                             </a>
                         </div>
                         <div class="mt-auto border-t-2 border-neutral-100 px-6 py-3 dark:border-neutral-600 dark:text-neutral-50">
-                            <small>Posted ${wiki.wiki_created_at} ?></small>
+                            <small>Posted ${wiki.wiki_created_at}</small>
                         </div>`;
 
                         parent.appendChild(div);
